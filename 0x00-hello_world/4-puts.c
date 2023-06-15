7db0e767858d353ba78332c414d13e896eb260b1
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-15 16:42:55 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: use function puts
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzlen");
+	return (0);
+}
