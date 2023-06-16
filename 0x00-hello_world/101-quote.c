c858d4481b9bc5f5626f68a7c9bb6cb3ac306977
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-16 04:23:25 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <sydio.h>
+
+/**
+ * main - write a program without using printf or put
+ *
+ * Return 1 error
+ *
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+	return (1);
+}
