700a2e54a38d1d3fc1a0c70e7be1f422c9aa00ad
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-15 17:28:13 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- *
  * main - using priting
- *
- * Return 0 (success)
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	Printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
