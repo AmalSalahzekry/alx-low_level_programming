f22bac9679137dd3d8ceacaa43397ff144bf8882
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-22 09:05:52 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Write a function that multiplies two integers.
+ * @a: first input
+ * @b: second input
+ * Return: sum
+ */
+
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
