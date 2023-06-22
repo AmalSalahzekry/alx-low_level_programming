3b86406f4f0766de71f036af72580ee26ce40a32
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-22 14:21:18 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,12 +7,21 @@
 
 void print_square(int size)
 {
-	int sq1, sq2;
-
-	for (sq1 = 1; sq1 < size; sq1++)
+	if (size <= 0)
 	{
-		for (sq2 = 1; sq2 < size; sq2++)
-			_putchar('#');
 		_putchar('\n');
+	}
+	else
+	{
+		int sq1, sq2;
+
+		for (sq1 = 0; sq1 < size; sq1++)
+		{
+			for (sq2 = 0; sq2 < size; sq2++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
