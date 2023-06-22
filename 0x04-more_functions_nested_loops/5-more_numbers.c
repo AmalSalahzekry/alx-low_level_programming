f25bac136dc4e5218a623e8e8996ac48675a098b
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-22 16:16:57 -0700

    more num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,15 @@ void more_numbers(void)
 	int row;
 	int num;
 
-	for (row = 1 ; row < 11; row++)
+	for (row = 1 ; row <= 10; row++)
 	{
-		for (num = 0; num < 15; num++)
+		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
+			if (num >= 10)
 			{
-				_putchar(1 + '0');
-				num = num % 10;
+				_putchar('1');
 			}
-			_putchar(num + '0');
+			_putchar(num % 10 + 49);
 		}
 		_putchar('\n');
 	}
