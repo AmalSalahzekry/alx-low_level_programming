334effed88d9a61f548fab0ef13cc9238c160f9c
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-30 05:08:33 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * @c: string
+ * Return: cp value
+ */
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char letr[] = {a, A, e, E, o, O, t, T, l, L};
+	int num[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1}
+	unsigned int m;
+
+	while (*c)
+	{
+		for (m = 0; m < sizeof(letr) / sizeof(char); m++)
+		{
+			if (*c == letr[m] || *c == letr[m] + 32)
+			{
+				*c = '0' + num[m];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
