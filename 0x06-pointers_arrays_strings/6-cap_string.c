ab5d83d59d2959fe5ff2823ee9dd635295d4a85f
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-30 03:53:35 -0700

    cap str

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,9 +8,8 @@
 
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i])
 	{
 		while (!(str(i) >= 'a' && str[i] <= 'z'))
@@ -22,7 +21,7 @@ char *cap_string(char *str)
 			str[i - 1] == '"' ||
 			str[i - 1] == '(' || str[i - 1] == ')' ||
 			str[i - 1] == '{' || str[i - 1] == '}' ||
-			str[i - 1] == 0)
+			i == 0)
 			str[i] -= 32;
 		i++;
 	}
