fe3150ebe289ee2f35700499b21bfbd6b1a5cb99
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-06-29 17:53:27 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,10 @@
 
 /**
  * _strcmp -  compares two strings
- * @s1:
- * @s2:
- * Return: 0 for success*/
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: 0 for success
+ */
 
 int _strcmp(char *s1, char *s2)
 {
