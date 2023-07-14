0343962fba9666ef8837c4deda35fd66386c2be7
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-07-14 05:08:03 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -35,7 +35,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == 0)
 		return (NULL);
 
-	_memset(m, 0, sizeof(int) * nmemb);
+	_memset(ptr, 0, sizeof(int) * nmemb);
 
 	return (ptr);
 }
