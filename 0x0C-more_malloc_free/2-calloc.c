53ad7323114bad0a1f1b57e8c4861d895152172e
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-07-14 05:01:43 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#includ "main.h"
+
+/**
+ * *memset - entry point
+ * @a: pointer
+ * @j: char to copy
+ * @n: bytes to use
+ * Return: pointer
+ */
+
+char *memset(char *a, char j, unsigned int n)
+{
+	char *s = a;
+
+	while (n--)
+		*a++ = j;
+
+	return (s);
+}
+/**
+ * *_calloc - allocates memory for an array, using malloc.
+ * @nmemb: elements on array
+ * @size: the size of elements
+ * Return: a pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	ptr = malloc(sizeof(int) * nmemb);
+
+	if (ptr == 0)
+		return (NULL);
+
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (ptr);
+}
