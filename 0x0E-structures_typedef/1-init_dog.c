7fe3097eac8a97072ac4728dbf6d0bb6c480a4da
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-07-18 15:45:03 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - init_dog
+ * @d: the dog pointer
+ * @name: the name
+ * @age: tha age
+ * @owner: who is the owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		*d.age = age;
+		*d.owner = owner;
+	}
+}
