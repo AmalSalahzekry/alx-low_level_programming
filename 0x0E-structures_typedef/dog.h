d256d64f79edcbf571a81615571be8068e7491c6
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-07-18 17:34:11 -0700

    new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,6 +23,8 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(const char *ptr);
+char *_strcopy(char *s, char *a);
 void free_dog(dog_t *d);
 
 #endif
