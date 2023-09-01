bc5f7e3678376bebe6cb96a78149ad9714143ed7
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-01 13:57:38 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		int mul = atoi(argv[1]) * atoi(argv[2]);
 
 		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
