40c7a0dea6916010a61094a5ba3e4cb34057b94f
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-01 13:32:45 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of strangs
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int sum;
+
+	if (argv != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		sum = num1 * num2;
+		printf("%d\n", sum);
+	}
+	return (0);
+}
+
