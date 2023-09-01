3a1e5f10053f14bc0916c6fee3a1dc0ac00d4b7b
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-01 12:56:27 -0700

    argc to print

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it.
+ * @argc: Number of stranings
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	void(argv[]);
+	Printf("%d\n", argc - 1);
+	return (0);
+}
