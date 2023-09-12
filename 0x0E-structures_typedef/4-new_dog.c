f486e8a9ca2392a48a6b83ac2b1f34116b1f7021
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-12 14:48:39 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -63,6 +63,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ptrd);
 		return (NULL);
 	}
+	ptrd->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (ptrd->owner == NULL)
+	{
+		free(ptrd->name);
+		free(ptrd);
+		return (NULL);
+	}
 
 	ptrd->name = _strcopy(ptrd->name, name);
 	ptrd->age = age;
