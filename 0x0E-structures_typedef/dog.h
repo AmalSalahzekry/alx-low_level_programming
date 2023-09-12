42e67a0b3d2ad9fa70edbda5a824d6f0e16f4d6c
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-12 10:05:27 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+/**
+ * struct dog - is astruct
+ * @name: the name
+ * @age: the age
+ * @owner: the owner name
+ */
+
+typedef struct dog
+{
+	char name;
+	int age;
+	char owner;
+} my_dog;
+
+#endif
