9c13ee69238e252bd244cfed24e55b80a29a470b
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-12 10:21:53 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 /**
  * struct dog - is astruct
@@ -18,9 +16,5 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
