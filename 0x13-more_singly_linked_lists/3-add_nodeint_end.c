903659552b336aff4543103cf7ced0f31f4f5181
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-26 16:48:08 -0700

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	end_node->next = NULL;
 	end_node->n = n;
 	if (*head == NULL)
-		*head == end_node;
+		*head = end_node;
 	else
 	{
 		num = *head;
