23b2bc318f62ae9b27f605b3f330364025e7bf7b
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-27 13:21:33 -0700

    free

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +17,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next;
+struct list_s *next;
 } list_t;
 
 int _putchar(char c);
