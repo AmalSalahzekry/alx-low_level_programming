bb6ad96a1481205fd0b3c8d9054c148f5456a02f
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-26 17:22:57 -0700

    pop listint

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists"
+
+/**
+ * pop_listint - delet node
+ * @head: node pointer
+ * Return: nodes data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *n_node;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	n_node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = n_node;
+	return (n);
+}
