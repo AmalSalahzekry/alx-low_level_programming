08f3e45cd9e636f1a24850bdb93e20d8cb52d1a1
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-26 17:43:46 -0700

    pop list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,14 +10,15 @@ int pop_listint(listint_t **head)
 {
 	listint_t *n_node;
 	int n;
+	listint_t *tmp;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 
 	n_node = (*head)->next;
 	n = (*head)->n;
-	free(*head);
-	if (n_node != NULL)
+	*tmp = *head;
+	free(tmp);
 	*head = n_node;
 	return (n);
 }
