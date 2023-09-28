9aa284435f5803df62142f7aec1aec036c229219
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-28 14:52:13 -0700

    git bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit -  function that sets the value of a bit to 1 at a given index.
+ * @n: the number
+ * @index: the bit
+ * Return: value of bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index);
+{
+	int value_b;
+
+	if (index > 63)
+		return (-1);
+
+	value_b = (n >> index) & 1;
+
+	return (value_b);
+}
